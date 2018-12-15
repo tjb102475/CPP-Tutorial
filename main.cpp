2cb542ce655473862e77dce9c42e9e9aeb7babee
tjb102475 <45901122+tjb102475@users.noreply.github.com>
2018-12-15 13:39:27 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+//Create program for user to input 3 scores
+
+using namespace std;
+
+int main()
+{
+   cout << "Enter First Player Score" << endl;
+   int firstScore;
+   cin >> firstScore;
+
+   cout << "Enter Second Player Score" << endl;
+   int secondScore;
+   cin >> secondScore;
+
+   cout << "Enter Third Player Score" << endl;
+   int thirdScore;
+   cin >> thirdScore;
+
+   cout << "First player score is:" << firstScore << endl;
+   cout << "Second player score is:" << secondScore << endl;
+   cout << "Third player score is:" << thirdScore << endl;
+
+    return 0;
+}
